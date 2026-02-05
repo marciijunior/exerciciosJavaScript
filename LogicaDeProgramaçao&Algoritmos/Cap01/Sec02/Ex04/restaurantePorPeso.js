a663efa949c6ff4d5d0261478ade2068bfd7c37d
Marcio Junior <marciojsjunior.mj@gmail.com>
2026-02-05 14:23:22 -0300

    lsdkfjslk

---
+++ LogicaDeProgramaçao&Algoritmos/Cap01/Sec02/Ex04/restaurantePorPeso.js
@@ -0,0 +1,10 @@
+const frm = document.querySelector("form");
+const respostaUm = document.querySelector("#respostaUm");
+
+frm.addEventListener("submit", (e) => {
+  const precoKg = Number(frm.querySelector("#idPrecoKg").value);
+  const quantidade = Number(frm.querySelector("#idQtdConsumo").value);
+  const consumoFinal = precoKg * quantidade;
+  respostaUm.innerText = `Valor a ser pago: R$ ${consumoFinal.toFixed(2)}`;
+  e.preventDefault();
+});
